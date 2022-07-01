c717265cdbb4bf6b672a7fc99d398597db481b98
DevoAlex <alex.peluso8@gmail.com>
2022-07-01 23:27:19 +0200

    modified the JS functions

--- src/script_dev.js
+++ src/script_dev.js
@@ -1,7 +1,28 @@
-async function requestSubjectData() {
-    const response = await fetch ("https://openlibrary.org/subjects/love.json")
-    const data = await response.json();
-    console.log(data);
-}
+import axios from 'axios';
 
-requestSubjectData();
+let arrayLibri =[]; //the array where i will put the book list
+
+//writing the function to call OpenLibrary API 
+let select = document.getElementById("srcCollectionInput");
+
+   async function requestSubjectData() {
+     output.innerHTML="";
+     let url_libri = `https://openlibrary.org/subjects/${select.options[select.selectedIndex].value}.json`;
+       try{
+        arrayLibri = await axios.get (url_libri);
+        let axiosLibri = arrayLibri.data.works;
+        console.log(axiosLibri);
+        //for loop to display the results of the array 
+        for (let i=0; i<=12; i++) {
+            document.getElementById("output").innerHTML+="<h2 id='title'>"+ axiosLibri[i].title + "</h2>";
+            }
+       } catch (e){
+        console.log (e);
+       } 
+    }
+
+//adding event listener to the button so he can call the function when pressed
+  let srcButton = document.getElementById("srcBtn");
+
+  srcButton.addEventListener('click', requestSubjectData);
+
